641fe64d661dc7edb92412b28cac77a95085b7d4
Aaron Boxer <boxerab@protonmail.com>
2025-11-14 14:28:06 -0500

    fuzzer: move corpus data into test data dir

--- test/fuzzers/build_seed_corpus.sh
+++ test/fuzzers/build_seed_corpus.sh
@@ -8,7 +8,7 @@ if [ "$OUT" == "" ]; then
 fi
 
 # Assuming a directory with CSS seed files (create manually or source from test data)
-SEED_DIR=$SRC/libcss-tests
+SEED_DIR=$SRC/libcss/test/data/css
 
 rm -f $OUT/css_parse_fuzzer_seed_corpus.zip
-zip $OUT/css_parse_fuzzer_seed_corpus.zip $SEED_DIR/vtt/*.vtt
+zip $OUT/css_parse_fuzzer_seed_corpus.zip $SEED_DIR/*.vtt $SEED_DIR/*.css
